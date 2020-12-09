c29496f5f057088131325b5ab9563446e81611f2
Dimasta123 <60709050+Dimasta123@users.noreply.github.com>
2020-12-09 18:26:18 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+class Student {
+  constructor(university, course, fullName) {
+    this.university = university;
+    this.course = course;
+    this.fullName = fullName;
+    this.marks = Array.from(
+      {length: 4},
+      () => Math.floor(Math.random() * (5 - 1 + 1)) + 1,
+    );
+    this.dismiss = true;
+  }
+  getInfo() {
+    return console.log(
+      `Student ${this.course} ${this.university} ${this.fullName}`,
+    );
+  }
+  getMarks() {
+    if (this.dismiss) {
+      return console.log(` ${this.fullName} имеет такие оценки ${this.marks} `);
+    } else {
+      return null;
+    }
+  }
+  set mark(num) {
+    if (this.dismiss) {
+      this.marks.push(num);
+    }
+  }
+  dismisser() {
+    return (this.dismiss = false);
+  }
+  recover() {
+    return (this.dismiss = true);
+  }
+  getAverageMark() {
+    return this.dismiss
+      ? this.marks.reduce((total, mark) => total + mark) / this.marks.length
+      : null;
+  }
+}
+class BudgetStudent extends Student {
+  constructor(university, course, fullName) {
+    super(university, course, fullName);
+    this.scholarShip = '1400 uh';
+    this.getScholarship = setInterval();
+
+    setInterval(
+      function getScholarships() {
+        if (this.dismiss && getAverageMark() < 4) {
+          return console.log(
+            `Добре навчаєтесь студент ${this.student} ви отримали стипендію в розмирі ${this.scholarShip}`,
+          );
+        }
+      },
+      30000,
+    );
+  }
+}
+const sasha = new Student('НТУ ХПИ', 3, 'Александр Мазюк');
